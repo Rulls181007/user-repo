c056b42c1bf963b0aeae02541c274924c13f8425
RullsHuman <akbar.2002.sy@gmail.com>
2025-02-01 16:37:22 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$dbname = "mini_googledb";
+$dbname = "mini_googledb.sql";
 
 // Membuat koneksi
 $conn = new mysqli($host, $username, $password, $dbname);
